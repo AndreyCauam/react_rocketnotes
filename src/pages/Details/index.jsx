ef4eef86e0d0068332805b331bbe7d79c9fd2e80
andrey <andreycauam@gmail.com>
2022-09-20 12:00:16 -0300

    started the project structure

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react"
+import "./style.css"
+
+export function Details() {
+    
+  return (
+    <Fragment>
+      <h1>Hello World!</h1>
+      <span>Ta pegando fogo!</span>
+    </Fragment>
+  )
+}
